bca651e71e0224f34a33f0f7bfa3ce31649c1bc7
Gincoolwant <soulbox790326@gmail.com>
2023-12-27 16:33:01 +0800

    feat: set up socket.io in clinet side & set up cors policy and listening events in Server

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const http = require('http').createServer()
-const io = require('socket.io')(http)
+const server = require('http').createServer()
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+})
 
 const PORT = 3000
-http.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
   console.log(`Listening on port ${PORT}...`)
 })
 
-io.on('connection', (socket) => {
-  console.log('user is connected.')
+io.on('connect', (socket) => {
+  console.log('user is connected.', socket.id)
 })
